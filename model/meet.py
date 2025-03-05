51a25c36168683950a6abb4325a0cda8ead8020a
vitor cortez <6536964+vccortez@users.noreply.github.com>
2025-03-05 08:40:10 -0300

    Atualiza modelo de aula

--- model/meet.py
+++ model/meet.py
@@ -1,12 +1,23 @@
 from pydantic import Field
-from typing import Optional
-from datetime import datetime
+from typing import Optional, List
+from datetime import date
 
 from .node import Node
 
 
 class Meet(Node):
-    """Encontro (aula) para um tópico"""
+    """Encontro sobre um ou mais tópicos"""
 
-    content: Optional[str] = Field(default=None)
-    date: Optional[datetime] = Field(default=None)
+    title: Optional[str] = Field(default=None)
+    description: Optional[str] = Field(default=None)
+    resources: List[str] = Field(default_factory=list)
+
+    content: Optional[bool] = Field(default=True)
+    canceled: Optional[bool] = Field(default=False)
+
+    start_date: Optional[date] = Field(default=None)
+    date_span: Optional[int] = Field(default=1)
+
+    module_number: Optional[int] = Field(default=1)
+    topics: List[str] = Field(default_factory=list)
+    dates: List[date] = Field(default_factory=list)
