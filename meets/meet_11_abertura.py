c0f49823597c5230db22b2bdc0d6fe6acfb4234f
vitor cortez <6536964+vccortez@users.noreply.github.com>
2025-03-15 11:04:13 -0300

    Atualiza recursos

--- meets/meet_11_abertura.py
+++ meets/meet_11_abertura.py
@@ -1,9 +1,11 @@
-from model import Meet
+from model import Meet, Resource
 
 meet = Meet(
     title="Abertura da disciplina",
     description="Apresentações; plano de ensino; ambiente de desenvolvimento; próximas aulas",
     has_content=False,
     date_span=2,
-    resources=["slides/11_abertura.qmd"],
+    resources=[
+        Resource(title="Apresentação", value="slides/11_abertura.qmd", kind="file"),
+    ],
 )
--- meets/meet_12_ciencia.py
+++ meets/meet_12_ciencia.py
@@ -1,8 +1,14 @@
-from model import Meet
+from model import Meet, Resource
 
 meet = Meet(
     title="Ciência de dados",
     description="Histórico; definições; KDD e CRISP-DM; Big Data; Data mining; processos de desenvolvimento",
-    resources=["anotacoes/12_intro.qmd"],
-    topics=["Introdução à ciência de dados", "Áreas e termos relacionados", "Processos de desenvolvimento"]
+    topics=[
+        "Introdução à ciência de dados",
+        "Áreas e termos relacionados",
+        "Processos de desenvolvimento",
+    ],
+    resources=[
+        Resource(title="Livro", value="[@carvalho:2024:cdfa, cap. 1--2]", kind="bib")
+    ],
 )
