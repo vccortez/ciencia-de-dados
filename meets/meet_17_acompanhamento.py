978de6e899ad3eb72167e0114aea8a54a588e6bb
vitor cortez <6536964+vccortez@users.noreply.github.com>
2025-04-12 21:47:45 -0300

    Atualiza calendário

---
+++ meets/meet_17_acompanhamento.py
@@ -0,0 +1,8 @@
+from model import Meet
+
+meet = Meet(
+    title="Acompanhamento do trabalho",
+    description="Intervalo reservado à resolução de dúvidas e conclusão do trabalho da primeira avaliação",
+    has_content=False,
+    date_span=3,
+)
